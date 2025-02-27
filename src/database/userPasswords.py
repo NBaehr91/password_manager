625fc57b653bfe86cfcd75462ca7a27e9759f778
NBaehr91 <baehr.niko@gmail.com>
2025-02-27 02:37:30 -0500

    Refactor password loading to return an empty dictionary instead of a list and enhance password saving logic to prevent duplicate emails for the same site

--- src/database/userPasswords.py
+++ src/database/userPasswords.py
@@ -11,13 +11,13 @@ def open_passwords(user):
     sanitized_user = "".join(c for c in user if c.isalnum() or c in (' ', '.', '_')).rstrip()
     USER_STORED_PWD = sanitized_user + "_passwords.json"
     if not os.path.exists(USER_STORED_PWD):
-        return []
+        return {}
 
     try:
         with open(USER_STORED_PWD, "r") as f:
             user_passwords = json.load(f)
     except json.JSONDecodeError:
-        return []
+        return {}
 
     return user_passwords
 
@@ -39,6 +39,9 @@ def save_password(user, site, email, password):
     if site not in user_passwords:
         # If the site is not in the dictionary, add it
         user_passwords[site] = []
+        user_passwords[site].append({"email": email, "password": password, "date": date_saved})
+    elif email not in user_passwords[site]:
+        # If the email is not in the list for the site, add it
         user_passwords[site].append({"email": email, "password": password, "date": date_saved})
     else:
         # If the site is already in the dictionary, update the email and password

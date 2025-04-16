9a35cc514a1ec93cce187ee7551bdc2a1abf2e3b
NBaehr91 <baehr.niko@gmail.com>
2025-04-16 19:56:53 -0400

    Implement user registration with optional 2FA support, including TOTP secret generation and UI enhancements for user input. Updates old users to enable 2FA on first login

--- src/UI/login.py
+++ src/UI/login.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+from UI.registerUser import RegisterNewUserPage
 from tkinter import messagebox
-from database.passwordStorage import check_master_password, check_user_exists, register_master_password
+from database.passwordStorage import check_master_password, check_user_exists, register_master_password, update_user_wth_2FA, update_user_wthout_2FA
 from UI.styles import BACKGROUND_COLOR, TEXT_COLOR, PRIMEARY_COLOR, SECONDARY_COLOR, get_fonts, on_hover, on_leave
-from security.encrypt import get_master_key
+from security.encrypt import get_master_key, generate_2FA_secret, verify_totp_code
+
 
 class LoginPage(tk.Frame):
     """Handles user login and registration."""
@@ -38,6 +40,37 @@ class LoginPage(tk.Frame):
         username = self.username_entry.get()
         password = self.password_entry.get()
         masterkey = get_master_key(password)
+        check_result = check_master_password(username, password)
+
+        if check_result == False:
+            messagebox.showerror("Error", "Invalid username or password.")
+            return
+        
+        match check_result["status"]:
+            case "update_user_2FA":
+                enable_2FA = self.ask_enable_2FA_popup()
+                if enable_2FA:
+                    # Here you would handle the enabling of 2FA
+                    secret_2FA_code = generate_2FA_secret()
+                    messagebox.showinfo("2Factor Authentication", f"2FA enabled. Your secret code is: {secret_2FA_code}\nPlease store it securely.")
+                    update_user_wth_2FA(username, secret_2FA_code)
+                else:
+                    update_user_wthout_2FA(username)
+                self.on_success(username, masterkey)  # Switch to the dashboard
+
+            case "2FA_needed":
+                # Here you would handle the 2FA verification process
+                totp_code = self.ask_totp_code()
+                if not verify_totp_code(check_result["totp_secret"], totp_code):
+                    messagebox.showerror("Error", "Invalid TOTP code.")
+                    return
+                messagebox.showinfo("Success", "2FA verified successfully!")
+                self.on_success(username, masterkey)  # Switch to the dashboard
+
+            case "success":
+                # If the user is already registered and no 2FA is needed
+                self.on_success(username, masterkey)  # Switch to the dashboard
+
         if check_master_password(username, password):
             messagebox.showinfo("Success", "Login successful!")
             self.on_success(username, masterkey)  # Switch to the dashboard
@@ -47,13 +80,47 @@ class LoginPage(tk.Frame):
     def register(self):
         """Checks if user exists and registers if not."""
        
-        username = self.username_entry.get()
-        password = self.password_entry.get()
+        new_user_window = RegisterNewUserPage(self.root)
+        self.wait_window(new_user_window)  # Wait for the registration window to close
 
-        if not check_user_exists(username):
-            register_master_password(username, password)
+        if hasattr(new_user_window, 'new_username') and new_user_window.new_username:
+            username = new_user_window.new_username
             messagebox.showinfo("Success", "New User Created!")
             self.on_success(username)  # Switch to the dashboard
 
-        else:
-            messagebox.showerror("Error", "User already exists.")
+    def ask_enable_2FA_popup(self):
+        """Asks the user if they want to enable 2FA."""
+        def on_yes():
+            nonlocal response
+            response = True
+            popup.destroy()
+        def on_no():
+            nonlocal response
+            response = False
+            popup.destroy()
+
+        response = None
+        popup = tk.Toplevel(self)
+        popup.title("Enable 2FA")
+        tk.Label(popup, text="Do you want to enable 2FA additional security?", font=get_fonts(self.root)["text"]).pack(pady=10)
+        tk.Button(popup, text="Yes", command=on_yes).pack(side=tk.LEFT, padx=20, pady=10)
+        tk.Button(popup, text="No", command=on_no).pack(side=tk.RIGHT, padx=20, pady=10)
+        return response
+    
+    def ask_totp_code(self):
+        """Asks the user to enter their TOTP code."""
+        def on_submit():
+            nonlocal totp_code
+            totp_code = totp_entry.get()
+            popup.destroy()
+
+        totp_code = None
+        popup = tk.Toplevel(self)
+        popup.title("Enter TOTP Code")
+        tk.Label(popup, text="Enter your TOTP code:", font=get_fonts(self.root)["text"]).pack(pady=10)
+        totp_entry = tk.Entry(popup, font=get_fonts(self.root)["text"])
+        totp_entry.pack(pady=5)
+        tk.Button(popup, text="Submit", command=on_submit).pack(pady=10)
+        popup.grab_set()
+        self.wait_window(popup)
+        return totp_code
